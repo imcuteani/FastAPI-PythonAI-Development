e46735fdbd0209d52c7d2b532d07ac76ef279645
anindita.basak@live.com <anindita.basak@live.com>
2026-02-10 12:00:10 +0530

    added SQLModel

--- JSONBody/items.py
+++ JSONBody/items.py
@@ -28,4 +28,4 @@ items = {
 
 @app.get("/items/{item_id}", response_model=Item)
 async def read_item(item_id: str):
-    return items(item_id)
+    return items[item_id]
--- books.py
+++ books.py
@@ -13,18 +13,7 @@ BOOKS = [
 ]
 
 
-@app.get("/books")
-async def read_all_books():
-     return BOOKS
-
-@app.get("/books/{book_title}")
-async def read_book(book_title: str):
-    for book in BOOKS:
-        if book.get('title').casefold() == book_title.casefold():
-            return book
 
 
-@app.post("/books/create_book")
-async def create_book(new_book=Body()):
-    BOOKS.append(new_book)
+
 

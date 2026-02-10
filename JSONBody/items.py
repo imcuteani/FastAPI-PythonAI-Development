d080374cfd9a74ea8151a1e0d94a30b64836f062
anindita.basak@live.com <anindita.basak@live.com>
2026-02-10 10:36:30 +0530

    added json body json_encoder

---
+++ JSONBody/items.py
@@ -0,0 +1,31 @@
+# Update the JSON encoding of FastAPI with HTTP PUT operation 
+
+# You can use the jsonable_encoder to convert the input data to the data which can be stored as JSON (e.g. with a NOSQL db)
+# for e.g. conversion of datetime to str type etc. 
+
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Declaration of class 
+
+class Item(BaseModel):
+    name: str | None = None
+    description: str | None = None
+    price: float | None = None
+    tax: float = 10.5 
+    tags: list[str] = []
+
+items = {
+    "foo": {"name": "Foo", "price": 50.2}, 
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.5, "tax": 10.5, "tag": []}, 
+} 
+
+# Decleration of Get decorator 
+
+@app.get("/items/{item_id}", response_model=Item)
+async def read_item(item_id: str):
+    return items(item_id)

a68319b5784c18ce04bf0595be2e44cc3a73fc47
anindita.basak@live.com <anindita.basak@live.com>
2026-02-11 10:10:27 +0530

    added Query param forbid field

--- QueryParameterModel/items.py
+++ QueryParameterModel/items.py
@@ -7,12 +7,19 @@ from pydantic import BaseModel, Field
 app = FastAPI()
 
 # Declare the class inherting the BaseModel class in Pydantic 
+# In some scenario, we can restrict the query parameters which we can specify to define in the query string. 
+# In Pydantic model configuration, it can be declared to use Pydantic model config to forbid any extra field in the query parameter
+
+
 
 class QueryParamModel(BaseModel):
+    model_config = {"extra": "forbid"}
     limit: int = Field(100, gt=0, le=100)
     offset: int = Field(0, ge=0)
     order_by: Literal["created_at", "updated_at"] = "created_at"
     tags: list[str] = []
+
+
 
 # inject the model function into the final endpoint function 
 

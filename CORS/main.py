f1080a04e8c5eda0350fb969ba21112853b4ff66
anindita.basak@live.com <anindita.basak@live.com>
2026-02-20 12:23:04 +0530

    added CORS updated arguments

--- CORS/main.py
+++ CORS/main.py
@@ -24,6 +24,15 @@ origins = [
 ]
 
 # invoking the add_middleware method to allow origins and methods for FastAPI 
+# FastAPI supports the following types of arguments - 
+
+# - allow_origins -> list of origins should be permitted to make cross-origin requests. The wildcard is supported but not recommended. 
+# - allow_origin_regex -> a regex string to match against origins which should be permitted to make cross-origin requests. 
+# - allow_headers -> list of HTTP headers which should be allowed to support for cross-origin requests. 
+# - allow_credentials -> defines the cookies to be supported for cross-origin request, default is False. 
+# - expose_headers -> indicate any response headers which should be made accessible to be browser. 
+# - max_age -> Sets up a max time limit in sec for browsers to cache the CORS responses. Default is 600 sec.  
+
 
 app.add_middleware(
     CORSMiddleware, 
